b69658e63fce4bb90eca752267d2f52b91552436
JohnMejia014 <johnmejia223@gmail.com>
2024-06-17 13:40:23 -0500

    individual components

---
+++ src/Tabs/Components/KickOffConnect.js
@@ -0,0 +1,25 @@
+// src/Project.js
+import React, { useState } from 'react';
+import './KickOffConnect.css';
+import kickoffImage from '../ProjectImages/KickOffConnectDiscoveryScreen.png'
+const KickOffConnect = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const project= 
+    {
+      title: "KickOff Connect Play Nearby",
+      description: "Capstone Full Stack App Developer",
+      details: "Led the development of a multi-sport pickup app using React Native, Expo, and Google Maps API Responsible for designing, creating, developing, and testing the front end of the app Utilized Amazon S3 for storing posts and profile pictures, and managed user and event information with DynamoDB Collaborated with a cross-functional team of students and mentors to implement features and ensure seamless integration of front end with backend systems",
+      imageUrl: kickoffImage,
+    };
+  const onClickListener = () => console.log("this is the project", project);;
+  
+  return (
+    <div className="project-item">
+      <img src={kickoffImage} alt={project.imageUrl} className="project-image" onClick={onClickListener} />
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+    </div>
+  );
+};
+
+export default KickOffConnect;
--- src/Tabs/Projects.js
+++ src/Tabs/Projects.js
@@ -1,25 +1,10 @@
 // src/Projects.js
 import React from 'react';
 import TabsDropdown from './TabBar';
-import ProjectComponent from './Components/ProjectComponent';
 import './Projects.css';  // Create a new CSS file for Projects component specific styles
-import appImage from ''
+import KickOffConnect from './Components/KickOffConnect';
 const Projects = () => {
-  const projectData = [
-    {
-      title: "KickOff Connect Play Nearby",
-      description: "Capstone Full Stack App Developer",
-      details: "Led the development of a multi-sport pickup app using React Native, Expo, and Google Maps API Responsible for designing, creating, developing, and testing the front end of the app Utilized Amazon S3 for storing posts and profile pictures, and managed user and event information with DynamoDB Collaborated with a cross-functional team of students and mentors to implement features and ensure seamless integration of front end with backend systems",
-      imageUrl: '../ProjectImages/KickOffConnectDiscoveryScreen.png',
-    },
-    {
-      title: "Project 2",
-      description: "This is a brief description of project 2.",
-      details: "Detailed information about project 2.",
-      imageUrl: "path_to_project2_image.jpg",
-    },
-    // Add more projects as needed
-  ];
+
   return (
     <div className="App" id="projects">
       <div className="App-header">
@@ -32,9 +17,7 @@ const Projects = () => {
           </h1>
         </div>
         <div className="projects-grid">
-          {projectData.map((project, index) => (
-            <ProjectComponent key={index} project={project} />
-          ))}
+          <KickOffConnect/>
         </div>
       </div>  
     </div>  

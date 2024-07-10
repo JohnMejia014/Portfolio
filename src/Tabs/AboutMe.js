0c37e67451f2c1d16c866958b803d8a44c50a590
JohnMejia014 <johnmejia223@gmail.com>
2024-07-09 23:05:12 -0500

    changed about me page

--- src/Tabs/AboutMe.js
+++ src/Tabs/AboutMe.js
@@ -13,14 +13,15 @@ const AboutMe = () => {
                     <div className='content'>
                         <h2>About Me</h2>
                         <span className='line'></span>
+                            <p>Hello I am John Mejia,</p>
                             <p> 
-                                Software Engineer skilled in both software development and hardware integration. Demonstrated proficiency in
+                                I am a Software Engineer skilled in both software development and hardware integration. Demonstrated proficiency in
                                 programming languages including Python, JavaScript, and C++, complemented by hands-on experience in circuit
                                 board programming and test bench creation. Utilizes a blend of front-end and back-end tools, such as React and
                                 MongoDB, to enhance product functionality and user experience, ensuring robust, scalable solutions in
                                 technology projects.
                             </p>
-                        <p>Hello I am John Mejia</p>
+                            <p>I attended the University of Texas and moved to the Austin area in 2007 when I was 5. I have a passion for sports, especially soccer, basketball, and football. I also enjoy running, going to the gym, and just about any outdoor activity. </p>
                     </div>
                 </div>
             </div>
